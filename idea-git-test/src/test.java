cc34ac0bccdec0fa352bcaa556a60ed8d02f5493
zoufeng <1270647715@qq.com>
2019-04-20 15:03:20 +0800

    创建test类

---
+++ idea-git-test/src/test.java
@@ -0,0 +1,12 @@
+/**
+ * @author 邹峰
+ * @title: test
+ * @projectName TxDemo
+ * @description: TODO
+ * @date 2019/4/20 15:02
+ */
+public class test {
+    public void show(){
+        System.out.println("xxx");
+    }
+}
